a5d7d65ddc92fec6a5cad72a6b26dc7631b5ee15
Fabrizio <fabgrel10@gmail.com>
2022-04-06 15:56:38 -0500

    ✨ feat: Add teacher class

---
+++ app/teacher.rb
@@ -0,0 +1,17 @@
+require_relative './person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+teacher = Teacher.new(30, name: 'John', parent_permission: false)
+puts teacher.can_use_services?
