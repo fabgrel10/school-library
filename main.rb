548545556eb087d2620e39425bba1b7de4f87725
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-11 21:55:33 -0500

    ✨ feat: Create relationship between Student and Classroom

--- main.rb
+++ main.rb
@@ -1,9 +1,4 @@
-require_relative 'app/person'
-require_relative 'app/base_decorator'
+require_relative 'app/student'
 
-person = Person.new(22, name: 'maximilianus')
-puts person.correct_name
-puts capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+student = Student.new(18, name: 'John')
+puts student.add_classroom('1A')
