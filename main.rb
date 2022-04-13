fc6d49c66897ef244b361654ff38f4b5bb9dea70
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-12 19:50:45 -0500

    🔧 chore: Commented instances in main to avoid linter errors

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative './app/student'
 require_relative './app/person'
 require_relative './app/book'
 
-classroom1 = Classroom.new('1A')
-student1 = Student.new(18, name: 'John')
-person1 = Person.new(18, name: 'John', parent_permission: true)
-book1 = Book.new('Harry Potter', 'J.K. Rowling')
+# classroom1 = Classroom.new('1A')
+# student1 = Student.new(18, name: 'John')
+# person1 = Person.new(18, name: 'John', parent_permission: true)
+# book1 = Book.new('Harry Potter', 'J.K. Rowling')
