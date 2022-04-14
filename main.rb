44dfa991e66d0a4c91610ba6d11b2cbb8b579cf2
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-13 20:48:57 -0500

    ✨ feat: Modify main class an an entry point
    Now main defines the entry point: by running 'rake run' a
    main method is called.

--- main.rb
+++ main.rb
@@ -1,2 +1,27 @@
 require_relative './app/app'
 
+def main
+  system 'clear'
+  puts '         ***********************'
+  puts '         * School Library v1.0 *'
+  puts '         ***********************'
+  puts
+
+  app = App.new
+  loop do
+    puts "Select an option:
+    1 - List available books
+    2 - List registered people
+    3 - Add a person
+    4 - Add a book
+    5 - Add a rental
+    6 - Show rentals
+    7 - Exit"
+    choice = gets.chomp.to_i
+    break if choice == 7
+
+    app.run(choice)
+  end
+  puts "Come back soon!\n\n"
+end
+main
