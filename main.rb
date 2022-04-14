18e9ebc46935d68a54ba0a9b6e9ec13cec1a1675
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-13 20:38:06 -0500

    ✨ feat: Create App and Validations classes

--- main.rb
+++ main.rb
@@ -1,9 +1,2 @@
-require_relative './app/classroom'
-require_relative './app/student'
-require_relative './app/person'
-require_relative './app/book'
+require_relative './app/app'
 
-# classroom1 = Classroom.new('1A')
-# student1 = Student.new(18, name: 'John')
-# person1 = Person.new(18, name: 'John', parent_permission: true)
-# book1 = Book.new('Harry Potter', 'J.K. Rowling')
